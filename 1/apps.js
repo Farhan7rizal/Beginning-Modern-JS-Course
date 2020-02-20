b2f1245c237754be5657678051d7fadb400cb55f
Farhan7rizal <44233031+Farhan7rizal@users.noreply.github.com>
2020-02-20 16:41:17 +0700

    beninngging and some console explanation code
    ya

---
+++ 1/apps.js
@@ -0,0 +1,21 @@
+// alert('Hi!');
+// Log to console
+console.log('this is console log from source code');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,4]); //array
+console.log({a:1, b:2}); //object
+console.table({a:1, b:2}); //table
+
+console.error('this is some error');
+console.clear();
+console.warn('this is a waring!');
+console.time('hello');
+    console.log(123);
+console.timeEnd('hello');
+
+/*
+
+*/
